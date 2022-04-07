84e6fecc112517038c3222e89203932181f0a3f9
'Miguel <'miguel.cobertera-criado@iessampedro.org'>
2022-04-07 18:04:33 +0200

    Creacion de Medusa y la instanciacion en clase Principal

---
+++ src/paquete1/Medusa.java
@@ -0,0 +1,24 @@
+package paquete1;
+
+public class Medusa extends Mascota{
+
+	private String especie;
+
+	public Medusa(String nombre, String especie) {
+		super(nombre);
+		this.especie = especie;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	public void mandarMensaje() {
+		System.out.println("Soy " + getNombre() + " y te voy a picar ¡¡¡¡¡¡¡PSSSIIIIIII!!!!!!");
+	}
+	
+}
--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -4,8 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		
 
-	
+		Medusa medusa1 = new Medusa("Huevo", "calavera portuguesa");
 
+		medusa1.mandarMensaje();
+		
 	}
 
 }
