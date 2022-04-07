48edf8f1026494deca3d5becaecadca262321e69
'Miguel <'miguel.cobertera-criado@iessampedro.org'>
2022-04-07 17:48:10 +0200

    Creacion del proyecto en Eclipse y el repositorio de github

---
+++ src/paquete1/Mascota.java
@@ -0,0 +1,24 @@
+package paquete1;
+
+public class Mascota {
+	
+	private String nombre;
+
+	public Mascota(String nombre) {
+		this.nombre = nombre;
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void mandarMensaje() {
+		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+	}
+
+}
