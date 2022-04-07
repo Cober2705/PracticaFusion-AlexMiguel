bed0a5d11adc547d88fd7ddee09b37bdf8b6b482
Alejandro Rodriguez <alejandrorodriguez@correo.com>
2022-04-07 18:01:43 +0200

    Creación de clase León, y la introducción de un León en la clase principal.

---
+++ src/paquete1/Leon.java
@@ -0,0 +1,24 @@
+package paquete1;
+
+public class Leon extends Mascota{
+	
+	private String sexo;
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Leon(String nombre, String sexo) {
+		super(nombre);
+		this.sexo = sexo;
+	}
+	
+	public void mandarMensaje() {
+		System.out.println("Soy el Leon " + getNombre() + " de sexo "+ this.sexo +" y hago ROOOAAAARRRRR");
+	}
+
+}
--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		
 
+		Leon leon1= new Leon("Balto","macho");
+		leon1.mandarMensaje();
 	
 
 	}
